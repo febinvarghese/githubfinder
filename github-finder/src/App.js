f361e5efabdca6a93df560eeea22d1cd3136a28b
febin <febin@eguidancelabs.com>
2022-02-09 22:44:22 +0530

    inital commit

---
+++ github-finder/src/App.js
@@ -0,0 +1,27 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "./components/layout/NavBar";
+import Footer from "./components/layout/Footer";
+import Home from "./pages/Home";
+import NotFound from "./pages/NotFound";
+import About from "./pages/About";
+
+function App() {
+  return (
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <NavBar />
+        <main className="container mx-auto px-3 pb-12">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/notfound" element={<NotFound />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
+
+export default App;
